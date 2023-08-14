5715f5181d64dca2a5586b51b75616bc73bb857d
d_c <15929329445@163.com>
2023-08-14 21:23:15 +0800

    feat 通过layout文件实现简单的根据路由的更新来局部更新页面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
 import Link from "next/link";
+import { useRouter } from 'next/navigation';
 import "./app.css";
 
 export default function Page() {
+  // const router = useRouter();
   return (
     <main>
-      <h1>这是跟路由对应的UI</h1>
-
-      <div className="linkWrapper">
-        <span>
-          <Link href="/dashboard">去dashboard页面</Link>
-        </span>
-        <span>
-          <Link href="/about">去about页面</Link>
-        </span>
-      </div>
-
-      <div className="linkContent">
-        <h2>这是路由对应的内容区域</h2>
-        <div></div>
-      </div>
     </main>
   );
 }
